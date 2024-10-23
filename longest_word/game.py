4cff375f2c4777001fdc2a7816a9839be4c99cd5
antoinechardigny <b00318588@essec.edu>
2024-10-23 14:04:37 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,15 +1,22 @@
+import requests
+
 class Game:
     def __init__(self) -> list:
         """Attribute a random grid to size 9"""
-        self.grid = ['G','R','E','Q','I','V','B','M','O']
+        self.grid = ['G','R','T','T','O','A','B','M','O']
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
         bool_ = set(word).issubset(set(self.grid))
-        return bool_
+        if bool_:
+            data = requests.get(f'https://dictionary.lewagon.com/{word}').json()
+            is_valid = data['found']
+        else:
+            is_valid = False
+        return is_valid
 
 if __name__ == "__main__":
     game = Game()
     print(game.grid) # --> OQUWRBAZE
-    my_word = 1000
+    my_word = 'TOMATO'
     print(game.is_valid(my_word)) # --> True
